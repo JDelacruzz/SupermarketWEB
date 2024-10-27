2097070fa05925742abdde24d68a3e9890c23988
Nomadin_Portatil <delacruzjuan07@hotmail.com>
2024-10-27 14:39:42 -0500

    Modelos y configuracion del DbContext

--- SupermarketWEB/Program.cs
+++ SupermarketWEB/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
 namespace SupermarketWEB
 {
     public class Program
@@ -8,6 +10,10 @@ namespace SupermarketWEB
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<SupermarketContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Supermarket"))
+                );
 
             var app = builder.Build();
 
